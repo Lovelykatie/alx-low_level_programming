89eb273bd394a4a20854c59fb8045605d21773d6
Lovelykatie <efonekate35@gmail.com>
2023-03-16 23:53:10 +0000

    c-programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function is used to print
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
