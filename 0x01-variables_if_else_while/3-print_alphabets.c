64036bb3fd11ffe721c7ba4437fa51521937bdbd
Lovelykatie <efonekate35@gmail.com>
2023-03-17 12:30:19 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints alphabets in lowercase
+ *
+ * Return: 0 successful
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
