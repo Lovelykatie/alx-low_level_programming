1917e7378d490f95754885c62ab8206d09b4778a
Lovelykatie <efonekate35@gmail.com>
2023-03-17 13:17:16 +0000

    single-digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to print single digits
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
